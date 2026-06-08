1f1c409f15d1d84fe6a4172e4b048b96b178b705
vlincinerate@gmail.com <vlincinerate@gmail.com>
2026-06-08 01:29:33 -0700

    add interpreter entry point to main, update build files

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 
-#include "utils/debug_utils.h"
-#include "lexer/lexer.h"
-#include "parser/parser.h"
-#include "test_suite/lexer_test_suite.h"
-#include "test_suite/parser_test_suite.h"
+#include "core/interpreter.h"
 
 int main(int argc, char** argv)
 {
-	Lexer lexer;
-	Parser parser;
+	Interpreter interpreter;
+	interpreter.run();
+	//interpreter.run_tests();
 
-	if (lexer.lex_file(PROJECT_ROOT"/examples/main.va"))
-	{
-		lexer.dump_tokens();
-		parser.parse_tokens(lexer.get_tokens_ptr());
-		std::cout << Utils::serialize_ast(parser.get_ast());
-	}
 
-	LexerTestSuite lexerTestSuite;
-	lexerTestSuite.run_suite("Lexer Test Suite");
+	//Lexer lexer;
+	//Parser parser;
 
-	ParserTestSuite parserTestSuite;
-	parserTestSuite.run_suite("Parser Test Suite");
+	//if (lexer.lex_file(PROJECT_ROOT"/examples/main.va"))
+	//{
+	//	lexer.dump_tokens();
+	//	parser.parse_tokens(lexer.get_tokens_ptr());
+	//	std::cout << Utils::serialize_ast(parser.get_ast());
+	//}
+
+	//LexerTestSuite lexerTestSuite;
+	//lexerTestSuite.run_suite("Lexer Test Suite");
+
+	//ParserTestSuite parserTestSuite;
+	//parserTestSuite.run_suite("Parser Test Suite");
 
 	// TODO: make this a command line arg so we only regenerate the snapshots when we want to
 	//parserTestSuite.regenerate_ast_snapshots();
